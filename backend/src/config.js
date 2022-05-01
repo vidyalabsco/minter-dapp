81d08cc654e31fff271c7b62328cc58b80dec3e9
Vidya Labs <vidyalabs.web3@gmail.com>
2022-05-01 17:49:50 +0200

    ok

--- backend/src/config.js
+++ backend/src/config.js
@@ -101,12 +101,12 @@ const TOKENS_PER_MINT = 100; // maximum number of NFTs a user can mint in a sing
 const PUBLIC_MINT_START_DATE = "2022-05-30T22:00:00+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE =  null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE =  "2022-05-01T22:00:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x42f38FF4D8bb2d0C349669ACabd71CFFE4E4AdF8"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = [0xD1360a0d5EfF74B59CE009569Faf536a1d13969B]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "0xC4457f603B1153A383E99304FB1A2eFfD6881C81"; // If you want to manually include it
