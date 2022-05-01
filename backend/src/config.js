4319ee5a3961493b9d6914ecf0986638dc713ef6
Vidya Labs <vidyalabs.web3@gmail.com>
2022-05-01 17:55:45 +0200

    ok

--- backend/src/config.js
+++ backend/src/config.js
@@ -101,7 +101,7 @@ const TOKENS_PER_MINT = 100; // maximum number of NFTs a user can mint in a sing
 const PUBLIC_MINT_START_DATE = "2022-05-30T22:00:00+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE =  "2022-05-01T22:00:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE =  "2022-04-30T22:00:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x42f38FF4D8bb2d0C349669ACabd71CFFE4E4AdF8"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
