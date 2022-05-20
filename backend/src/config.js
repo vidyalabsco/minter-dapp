b357b422fea0dd8a6b6a9a51d1dbbf667b81b6d7
Vidya Labs <vidyalabs.web3@gmail.com>
2022-05-20 17:49:37 +0200

    ok

--- backend/src/config.js
+++ backend/src/config.js
@@ -98,7 +98,7 @@ const MINT_PRICE = 180; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC
 const TOKENS_PER_MINT = 50; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-05-30T22:00:00+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-05-29T22:00:00+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE =  null; // Optional. Eg: 2022-02-08T11:30:48+00:00
