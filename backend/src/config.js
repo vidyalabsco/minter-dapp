8bc0e0aba8d837e288e6c402bbe7ce52a9ec0ab3
Vidya Labs <vidyalabs.web3@gmail.com>
2022-05-30 20:15:17 +0200

    yes

--- backend/src/config.js
+++ backend/src/config.js
@@ -109,7 +109,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES =  null; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "0x1Fb4EfE599f1E53A53C6430D02Fccd8b752B6e4C"; // If you want to manually include it
+let CONTRACT_ADDRESS = ""; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
